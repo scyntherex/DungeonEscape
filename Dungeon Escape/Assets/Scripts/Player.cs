35397646840b9e7df734da7d890ffd7a019bfbaa
Rone <rone.caballero@gmail.com>
2019-01-27 16:23:41 -0800

    Player script attached to player object.

---
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    //get handle to rigidbody
+	// Use this for initialization
+	void Start () {
+	    //assign handle of rigidbody	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Horizontal inputs for left/right
+
+        //current velocity = 
+        //new velocity (horizontal input, current velocity.y);
+	}
+}
