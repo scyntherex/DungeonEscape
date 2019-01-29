f6bde1bde9cb703ce71967187a59880ffdfe2324
Rone <rone.caballero@gmail.com>
2019-01-28 19:13:21 -0800

    Implemented Horizontal Player movement.

--- Dungeon Escape/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -6,16 +6,24 @@ public class Player : MonoBehaviour {
 
 
     //get handle to rigidbody
+    private Rigidbody2D rb;
+
+
+    //private bool moving = false;
+
 	// Use this for initialization
 	void Start () {
-	    //assign handle of rigidbody	
-	}
+        //assign handle of rigidbody
+        rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		//Horizontal inputs for left/right
-
+	void Update ()
+    {
+        //Horizontal inputs for left/right
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
         //current velocity = 
         //new velocity (horizontal input, current velocity.y);
-	}
+        rb.velocity = new Vector2(horizontalInput, rb.velocity.y);
+    }
 }
