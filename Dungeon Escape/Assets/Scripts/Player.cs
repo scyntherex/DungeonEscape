74424e57ae4741e248a8a0484f3cd7bdf621111b
Rone <rone.caballero@gmail.com>
2019-01-29 19:35:57 -0800

    Implemented Player jump mechanics.

--- Dungeon Escape/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -8,11 +8,17 @@ public class Player : MonoBehaviour {
     //get handle to rigidbody
     private Rigidbody2D rb;
 
+    //variale for jumpforce
+    [SerializeField]
+    private float liftForce = 5.0f;
 
-    //private bool moving = false;
+    [SerializeField]
+    private LayerMask groundLayer;
 
-	// Use this for initialization
-	void Start () {
+    private bool resetJumpNeeded = false;
+
+    // Use this for initialization
+    void Start () {
         //assign handle of rigidbody
         rb = GetComponent<Rigidbody2D>();
     }
@@ -20,10 +26,39 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        //Horizontal inputs for left/right
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        //current velocity = 
-        //new velocity (horizontal input, current velocity.y);
-        rb.velocity = new Vector2(horizontalInput, rb.velocity.y);
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(move, rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, liftForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        resetJumpNeeded = true;
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 
+            0.6f, groundLayer.value);
+        if(hit.collider != null)
+        {
+            if(resetJumpNeeded == false)
+                return true;
+        }
+
+        return false;
     }
 }
