b44653f8d2a9990a79cd0cbbb0e658d935731e98
scyntherex <rone.caballero@gmail.com>
2019-02-18 19:17:50 -0800

    Implemented idle animation to spider enemy once it reaches an endpoint.

--- Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
@@ -6,30 +6,43 @@ public class Spider : Enemy {
 
     private Vector3 target;
 
+    private Animator spiderAnim;
+
     public void Start()
-    { 
+    {
+        spiderAnim = GetComponentInChildren<Animator>();
     }
 
     public override void Update()
+    {
+        if (this.spiderAnim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Movement();
+    }
+
+    void Movement()
     {
         float step = speed * Time.deltaTime;
         //if current pos == point A
         //move to point B
         // else if current pos == pos B
         //move to point A
-        if(transform.position == pointA.position)
+        if (transform.position == pointA.position)
         {
             target = pointB.position;
+            spiderAnim.SetTrigger("Idle");
         }
         else if (transform.position == pointB.position)
         {
             target = pointA.position;
+            spiderAnim.SetTrigger("Idle");
         }
 
         transform.position = Vector3.MoveTowards(transform.position,
             target, step);
 
-        //play idle when destination reached
         //flip sprite
     }
 }
