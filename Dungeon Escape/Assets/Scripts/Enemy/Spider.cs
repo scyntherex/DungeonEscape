95a77743303081def484ec43dfe69d56ce4ed032
scyntherex <rone.caballero@gmail.com>
2019-02-18 19:06:05 -0800

    Implemented movement between two points for spider enemy.

--- Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spider : Enemy {
+
+    private Vector3 target;
+
     public void Start()
     { 
     }
 
-    public override void Attack()
-    { 
-        Debug.Log("Spider Attack!");
-    }
-
     public override void Update()
     {
-        Debug.Log("Spider Upadting...");
+        float step = speed * Time.deltaTime;
+        //if current pos == point A
+        //move to point B
+        // else if current pos == pos B
+        //move to point A
+        if(transform.position == pointA.position)
+        {
+            target = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            target = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position,
+            target, step);
+
+        //play idle when destination reached
+        //flip sprite
     }
 }
