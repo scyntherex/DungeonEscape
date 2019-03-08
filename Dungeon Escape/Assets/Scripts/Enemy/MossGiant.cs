7749e010bb19e75866c0b3fc425803549a95985c
scyntherex <rone.caballero@gmail.com>
2019-03-08 14:35:46 -0800

    Implemented comabt logic for Moss giant.

--- Dungeon Escape/Assets/Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/MossGiant.cs
@@ -10,9 +10,41 @@ public class MossGiant : Enemy, IDamageable
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
+    }
+
+    public override void Movement()
+    {
+        base.Movement();
+
+        Vector3 direction = player.transform.localPosition
+            - transform.localPosition;
+        //Debug.Log("Side: " + direction);
+
+        if (direction.x < 0 && anim.GetBool("InCombat") == true)
+        {
+            sprite.flipX = true;
+        }
+        else if (direction.x > 0 && anim.GetBool("InCombat") == true)
+        {
+            sprite.flipX = false;
+        }
     }
 
     public void Damage()
     {
+        //subtract 1 from health
+        Health--;
+
+        anim.SetTrigger("Hit");
+        isHit = true;
+        anim.SetBool("InCombat", true);
+
+        //if health < 1, destroy object
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
