71ff3482b2b5f72148ad1e3c46d68daa7901c878
scyntherex <rone.caballero@gmail.com>
2019-03-22 16:18:30 -0700

    Implemented Buy button logic.

--- Dungeon Escape/Assets/Scripts/Shop/Shop.cs
+++ Dungeon Escape/Assets/Scripts/Shop/Shop.cs
@@ -6,11 +6,16 @@ public class Shop : MonoBehaviour {
 
     public GameObject shopPanel;
 
+    //variable for current item selected.
+    public int itemSelected;
+    public int selectedCost;
+
+    private Player player;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            Player player = other.GetComponent<Player>();
+            player = other.GetComponent<Player>();
             if(player != null)
             {
                 UIManager.UIinstance.OpenShop(player.Gems);
@@ -35,13 +40,42 @@ public class Shop : MonoBehaviour {
         {
             case 0: //0 = flame sword
                 UIManager.UIinstance.UpdateShopSelection(62);
+                itemSelected = 0;
+                selectedCost = 200;
                 break;
             case 1: //1 = boots of flight
                 UIManager.UIinstance.UpdateShopSelection(-38);
+                itemSelected = 1;
+                selectedCost = 400;
                 break;
             case 2: //2 = castle key
                 UIManager.UIinstance.UpdateShopSelection(-153);
+                itemSelected = 2;
+                selectedCost = 100;
                 break;
+        }
+    }
+
+    //BuyItem  method
+
+    
+    
+    public void BuyItem()
+    {
+        //check if player gems is >= item cost.
+        if (player.Gems >= selectedCost)
+        {
+            //if it is, award item (subtract cost from player's gems.
+            player.Gems -= selectedCost;
+            Debug.Log("Purcahsed: " + itemSelected + ". " + player.Gems 
+                + " remaining.");
+            shopPanel.SetActive(false);
+        }
+        else
+        {
+            //else close shop
+            Debug.Log("Not enough gems!");
+            shopPanel.SetActive(false);
         }
     }
 }
