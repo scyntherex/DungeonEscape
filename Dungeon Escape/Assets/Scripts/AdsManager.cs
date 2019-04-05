af5398cd861e8dd8b0f4a690c6aa66cf456defc3
scyntherex <rone.caballero@gmail.com>
2019-04-04 19:46:36 -0700

    Implemented Unity ads manager.

---
+++ Dungeon Escape/Assets/Scripts/AdsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+    public void ShowRewardedAd()
+    {
+        Debug.Log("Showing Rewarded Ad.");
+
+        //Check if ad is ready
+        //Show(rewardedVideo)
+
+        if(Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions {
+                resultCallback = HandleShowResult
+            };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    void HandleShowResult(ShowResult result)
+    {
+        switch(result)
+        {
+            case ShowResult.Finished:
+                //award 100 gems
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Skipped ad yields no reward.");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Video no ready");
+                break;
+        }
+    }
+}
